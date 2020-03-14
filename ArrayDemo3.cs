c52a81226be2e24fbd24827fc010371e4d87ddde
Junny22-G <57775050+Junny22-G@users.noreply.github.com>
2020-03-14 19:55:19 +0800

    Add project files.

---
+++ ArrayDemo3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace prac_mode_3
+{
+    class ArrayDemo3
+    {
+        static void  Main()
+        {
+                double[] payRate = { 13.00, 17.35, 19.12, 22.45 };
+
+                for (int x = 0; x < payRate.Length; ++x)
+                    WriteLine("Pay rate{0} is {1}", x, payRate[x].ToString("C"));
+            ReadKey();
+        }
+    }
+}
---
+++ FindPriceWithWhileLoop.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace prac_mode_3
+{
+    class FindPriceWithWhileLoop
+    {
+        static void Main()
+        {
+            int x;
+            string inputString;
+            int itemOrdered;
+            double itemPrice = 0;
+            bool isValidItem = false;
+            int[] validValues = { 101, 108, 201, 213, 266, 304, 311, 409, 411, 412 };
+            double[] prices = {0.89, 1.23, 3.50, 0.69, 5.79, 3.19, 0.99, 0.89, 1.26, 8.00 };
+            Write("Enter item number ");
+            inputString = ReadLine();
+            itemOrdered = Convert.ToInt32(inputString);
+            x = 0;
+            while (x < validValues.Length && itemOrdered != validValues[x])
+                ++x;
+            if(x!=validValues.Length)
+            {
+                isValidItem = true;
+                itemPrice = prices[x];
+            }
+            if (isValidItem)
+                WriteLine("Item {0} sells for {1}", itemOrdered, itemPrice.ToString("C"));
+            else
+
+                WriteLine("No such item as {0}", itemOrdered);
+
+            ReadKey();
+        }
+    }
+   
+}
